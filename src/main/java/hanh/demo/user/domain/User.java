56af63f07e27f519737f173e5dbc31e8e386d0ba
Jin409 <sualng123@gmail.com>
2022-10-03 15:49:05 +0900

    add: user 도메인 추가

--- src/main/java/hanh/demo/user/domain/User.java
+++ src/main/java/hanh/demo/user/domain/User.java
@@ -1,2 +1,22 @@
-package hanh.demo.user.domain;public class User {
+package hanh.demo.user.domain;
+
+import hanh.demo.habit.domain.Habit;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @OneToMany(mappedBy = "user")
+    private List<Habit> habitList = new ArrayList<>();
+
 }
