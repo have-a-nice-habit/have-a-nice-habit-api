1bd9eb774a9cddec943094ad9c30ffce7cd3237e
Jin409 <sualng123@gmail.com>
2022-10-04 00:04:46 +0900

    chore: mock data 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into user (id, nickname)
+values (1, 'ALOE');
+insert into user (id, nickname)
+values (2, 'Zenol');
+insert into user (id, nickname)
+values (3, 'TRUFFLE');
+insert into user (id, nickname)
+values (4, 'Nuddy');
+insert into user (id, nickname)
+values (5, 'Alprazolam');
+insert into user (id, nickname)
+values (6, 'Diabetes');
+insert into user (id, nickname)
+values (7, 'SPF');
+insert into user (id, nickname)
+values (8, 'Clonidine');
+insert into user (id, nickname)
+values (9, 'Varibar');
+insert into user (id, nickname)
+values (10, 'Povidone');
+
+insert into habit (id, user_id, count, title) values (1, 1, 1, '물 마시기');
+insert into habit (id, user_id, count, title) values (2, 2, 7, '22시 이전에 자기');
