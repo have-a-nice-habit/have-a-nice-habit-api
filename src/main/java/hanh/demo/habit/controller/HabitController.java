e64629e5df696f3ed0412cc76b2b9da67a1b157d
Jin409 <sualng123@gmail.com>
2022-10-04 00:26:28 +0900

    chore: 상태 변경 테스트

--- src/main/java/hanh/demo/habit/controller/HabitController.java
+++ src/main/java/hanh/demo/habit/controller/HabitController.java
@@ -5,11 +5,9 @@ import hanh.demo.habit.domain.Habit;
 import hanh.demo.habit.dto.HabitRequestDto;
 import hanh.demo.habit.repository.HabitRepository;
 import hanh.demo.habit.service.HabitService;
-import hanh.demo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +23,7 @@ public class HabitController {
     private final HabitService habitService;
     private final HabitRepository habitRepository;
 
-    @PostMapping("/change-status")
+    @PostMapping("{habitId}/change-status")
     public ResponseEntity changeStatus(@PathVariable Long habitId){
         Optional<Habit> findHabit = habitRepository.findById(habitId);
 
@@ -35,7 +33,7 @@ public class HabitController {
             throw new DataNotFoundException();
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body("습관의 상태를 변경했습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body("상태를 변경했습니다.");
 
     }
 
