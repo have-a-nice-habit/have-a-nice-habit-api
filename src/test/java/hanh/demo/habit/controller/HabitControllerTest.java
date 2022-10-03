056e7b9a136ea342638e0680dd8b58cce2e4e9d7
Jin409 <sualng123@gmail.com>
2022-10-04 02:01:00 +0900

    chore: 달성 여부 표시 테스트

--- src/test/java/hanh/demo/habit/controller/HabitControllerTest.java
+++ src/test/java/hanh/demo/habit/controller/HabitControllerTest.java
@@ -14,13 +14,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.not;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -101,4 +112,48 @@ public class HabitControllerTest {
         Assertions.assertNotEquals(temp, habit.getIsDisplay());
 
     }
+
+    @Test
+    void addDateTest() throws Exception{
+
+        //given
+        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        String dateToStr = dateFormat.format(date);
+
+        User testUser = new User();
+        userRepository.save(testUser);
+
+        Habit habit = new Habit();
+        habit.setTitle("테스트용입니다");
+        habit.setCount(1);
+        habit.setUser(testUser);
+
+        Habit savedHabit = habitRepository.save(habit);
+
+        Long habitId = savedHabit.getId();
+
+        info.add("date",dateToStr);
+
+        // 추가 로직
+
+        //when
+        mockMvc.perform(post("/habit/"+habitId+"/add-date")
+                .params(info));
+
+        assertEquals(1, savedHabit.getDateList().size());
+        //then
+        assertThat(savedHabit.getDateList()
+                .contains(dateToStr));
+
+        // 취소 로직
+        mockMvc.perform(post("/habit/"+habitId+"/add-date")
+                .params(info));
+
+        assertEquals(0, savedHabit.getDateList().size());
+    }
+
+
 }
