91dfda45b3cf86c3c10e07fd2bc328d368d1be2b
Jin409 <sualng123@gmail.com>
2022-10-04 17:24:41 +0900

    add: HabitResponseDto 에 isAchieved 컬럼 추가

--- src/main/java/hanh/demo/habit/dto/HabitResponseDto.java
+++ src/main/java/hanh/demo/habit/dto/HabitResponseDto.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Getter
 public class HabitResponseDto {
@@ -13,6 +16,9 @@ public class HabitResponseDto {
     private Long id;
 
     private boolean isDisplay;
+
+    // 오늘 했는지 안했는지 확인
+    private boolean isAchieved;
 
     private Integer count;
 
@@ -23,6 +29,14 @@ public class HabitResponseDto {
         this.id = habit.getId();
         this.isDisplay = habit.getIsDisplay();
         this.count = habit.getCount();
+        this.isAchieved = getIsAchieved(habit.getDateList());
+    }
+
+    private boolean getIsAchieved(
+            List<Date> dateList
+    ){
+        Date now = new Date();
+        return dateList.contains(now);
     }
 
 }
