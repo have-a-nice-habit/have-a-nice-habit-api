75de1ae0da8becd25df1ce0a4efed048d01a071f
Jin409 <sualng123@gmail.com>
2022-10-14 23:54:52 +0900

    add: UserRequestDto 생성

--- src/main/java/com/hanh/web/dto/UserRequestDto.java
+++ src/main/java/com/hanh/web/dto/UserRequestDto.java
@@ -1,2 +1,37 @@
-package com.hanh.web.dto;public class UserRequestDto {
+package com.hanh.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@RequiredArgsConstructor
+@Getter
+public class UserRequestDto {
+    //닉네임은 최소 3자, 최대 10자
+    @Size(min = 3, max=10)
+    @NotEmpty(message = "닉네임은 필수항목입니다.")
+    private String nickname;
+
+    @Email
+    @NotEmpty(message = "이메일은 필수항목입니다.")
+    private String email;
+
+    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    private String password;
+
+    @Builder
+    public void UserRequestDto(
+            String nickname,
+            String email,
+            String password
+    ){
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
 }
