944d6bf6782d889d54eb07636dc1af61b2375c19
Jin409 <sualng123@gmail.com>
2022-10-14 23:54:35 +0900

    fix: user 모델 수정

--- src/main/java/com/hanh/domain/user/User.java
+++ src/main/java/com/hanh/domain/user/User.java
@@ -3,6 +3,7 @@ package com.hanh.domain.user;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hanh.domain.habit.Habit;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -13,15 +14,21 @@ import java.util.List;
 @Getter
 @Setter
 public class User {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 닉네임
     @Column(unique = true)
     private String nickname;
+
+    // 로그인에 사용되는 이메일
+    @Column(unique = true)
+    private String email;
+
+    // 로그인에 사용되는 비밀번호 -> oauth 적용시 수정 예정
+    private String password;
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Habit> habitList = new ArrayList<>();
-
 }
